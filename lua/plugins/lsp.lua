d6c591bcfbae17b78a712fcd3facaf7a789a83c3
PaulUlanovskij <99551153+PaulUlanovskij@users.noreply.github.com>
2024-07-05 09:18:57 +0300

    update

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -20,13 +20,19 @@ return {
       local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
       local lspconfig = require("lspconfig")
-	lspconfig.zls.setup({
-capabilities = capabilities
-			})
+      lspconfig.zls.setup({
+        capabilities = capabilities
+      })
       lspconfig.lua_ls.setup({
         capabilities = capabilities
       })
-
+      lspconfig.csharp_ls.setup({
+	      capabilities = capabilities,
+	      cmd = { "/home/paviel/.dotnet/tools/csharp-ls" },
+      })
+      lspconfig.clangd.setup({
+        capabilities = capabilities
+      })
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
       vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
